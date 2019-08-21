46bdbf10923a6682a54940a89f4860f5e4e4f42d
syuna9616 <syuna9616@gmail.com>
2019-08-22 01:54:01 +0900

    #20 コメント削除機能を実装 ①コメント削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@
 
 Rails.application.routes.draw do
   resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :comments, only: [:create]
+  resources :comments, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

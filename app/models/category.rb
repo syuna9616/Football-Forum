82b5c74fb97608088e98e982944e84d361449163
syuna9616 <syuna9616@gmail.com>
2019-08-22 23:55:28 +0900

    #24 dependentオプション追加 ①boardモデルにてcommentsデーブルに対するdependentオプション(delete_all)を追加しました ②categoryモデルにてboard_category_relationsテーブルに対するdependentオプション(delete_all)を追加しました

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,6 +9,6 @@
 #
 
 class Category < ApplicationRecord
-  has_many :board_category_relations
-  has_many :boards, through: :board_category_relations 
+  has_many :board_category_relations, dependent: :delete_all
+  has_many :boards, through: :board_category_relations
 end

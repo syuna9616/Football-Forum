d17a4387717674c616f1173a052e43f518843ff7
syuna9616 <syuna9616@gmail.com>
2019-08-27 03:22:13 +0900

    #27 テーブル、モデル等を修正 ①前回、選択できるカテゴリーを択一にしたのでテーブルの構造を１対多に変更しました ②①に伴うビューの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,9 +6,17 @@
 #  name       :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  board_id   :bigint
+#
+# Indexes
+#
+#  index_categories_on_board_id  (board_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (board_id => boards.id)
 #
 
 class Category < ApplicationRecord
-  has_many :board_category_relations, dependent: :delete_all
-  has_many :boards, through: :board_category_relations
+  has_many :boards
 end

f5bedc23954b0cad5f9b6160ab0136fdced7fb93
syuna9616 <syuna9616@gmail.com>
2019-10-03 00:12:13 +0900

    #45 アップローダーの設定を変更 ①アップローダーの設定を変更（ ・縦横比を維持したままwidth, heightを50pxにリサイズ ・JPGで保存 ・jpg,jpeg,gif,pngのみアップロード可能 ・ファイル名を日付に変更し拡張子をjpgに）

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -30,18 +30,26 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  process resize_to_fit: [50, 50]
   # end
-
+  process :convert => 'jpg'
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "something.jpg" if original_filename
+  end
+
+  def filename
+    if original_filename.present?
+      time = Time.now
+      name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+      name.downcase
+    end
+  end
 end

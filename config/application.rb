ed8a56ac90e259aa6652a7fc8bb469fc4cd2f646
syuna9616 <syuna9616@gmail.com>
2019-10-03 02:30:45 +0900

    #47 プロフィール画像の投稿機能を実装 ①アップローダーの設定ファイル、モデルへのマウント記述を修正しました ②ユーザー編集アクションのストロングパラメータにimageを追加 ③ユーザー登録、編集ページに画像選択フォームを追加 ④localファイルの細かい修正

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module App
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = "Tokyo"
-    config.i18n.default_locale = :ja 
+    config.i18n.default_locale = :ja
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
 end

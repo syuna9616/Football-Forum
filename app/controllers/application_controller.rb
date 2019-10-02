5d0aa844b26a0936f54f1d573dd8bf2924defd40
syuna9616 <syuna9616@gmail.com>
2019-10-03 00:30:50 +0900

    #46 usersテーブルにimageカラムを追加 ①usersテーブルにimageカラムを追加 ②deviseのストロングパラメータにimageを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :image])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:user_name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
     end
--- test/fixtures/users.yml
+++ test/fixtures/users.yml
@@ -8,6 +8,7 @@
 #  confirmed_at           :datetime
 #  email                  :string(255)      default(""), not null
 #  encrypted_password     :string(255)      default(""), not null
+#  image                  :string(255)
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string(255)
